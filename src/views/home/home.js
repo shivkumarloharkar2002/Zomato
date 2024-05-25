de9f76c62023d1023982e4cf9361aaaa1be87cfd
shiv <shivkumarloharkar2002@gmail.com>
2024-05-25 15:00:44 +0530

    homeview

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,15 +1,15 @@
 import './home.css'
+
 import Herosection from '../../components/home/HeroSection/Herosection';
 import Div3 from '../../components/home/div3/div3';
 import Footer from '../../components/home/footer/footer';
 
-function Home(){
+export default function Home(){
     return(
         <>
-		<Herosection/>
-        <Div3 />
-		<Footer/> 
+        <Herosection/>
+        <Div3/>
+        <Footer/>
         </>
-    )
+    );
 }
-export default Home;
