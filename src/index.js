110a4862b289a75d819eef84e583eac54212a016
Isha80 <102660111+Isha80@users.noreply.github.com>
2024-05-25 14:15:42 +0530

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './views/home/home'
-
+import Home from './views/home/home';
+import Investor from '../src/views/investor/investor';
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <>
     <Home />
+    <Investor />
+    </>
+
 );
---
+++ src/views/investor/investor.js
@@ -0,0 +1,16 @@
+import './investor.css';
+import Navbar from '../../components/Investor/Navbar';
+
+
+ export default function Investor(){
+    return(
+        <>
+        <Navbar/>
+
+       </>
+    )
+}
+
+
+ 
+
