6055c4ef774c4b379b09e3686ffd112ccc77707b
pradnya <pradnya9022@gmail.com>
2024-05-25 04:42:06 -0600

    newly code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Log from './views/Log/Log';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <>
