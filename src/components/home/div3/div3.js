17898e18102a772b051523dbbd98fe84fc260c46
shiv <shivkumarloharkar2002@gmail.com>
2024-05-26 12:14:13 +0530

    routing

--- src/components/home/div3/div3.js
+++ src/components/home/div3/div3.js
@@ -34,5 +34,5 @@ export default function Div3() {
 
             </div>
         </>
-    )
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './views/home/home';
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+const routes=createBrowserRouter([
+    {
+        path:"/",
+        element:<Home/>
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Home/>
+    <RouterProvider router={routes}/>
 );
