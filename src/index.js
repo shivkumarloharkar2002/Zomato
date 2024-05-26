17218c0dc09566b5b9342cbddc611064d9f5bed0
shiv <shivkumarloharkar2002@gmail.com>
2024-05-26 13:33:26 +0530

    div 3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './views/home/home';
+// import Investor from './views/investor/investor'
+// import Resto from './views/addrestaurant/resto';
+
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Log from './views/Log/Log';
+
 const routes=createBrowserRouter([
     {
         path:"/",
         element:<Home/>
     },
+    // {
+    //     path:'/investor',
+    //     element:<Investor/>
+    // },
+    // {
+    //     path:"/Resto",
+    //     element:<resto/>
+    // }
+    {
+        path:'/log',
+        element:<Log/>
+    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
