09c213176029ef4aef939252067b6aa115a3e78d
Isha80 <102660111+Isha80@users.noreply.github.com>
2024-05-27 13:23:40 +0530

    added comment2

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './views/home/home'
-import Home from './views/home/home';
-import Log from './views/Log/Log';
+// import Home from './views/home/home'
+// import Home from './views/home/home';
+ import Log from './views/Log/Log';
 
 
-import Home from './views/home/home';
-// import Investor from './views/investor/investor'
-// import Resto from './views/addrestaurant/resto';
 
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import Log from './views/Log/Log';
 
-const routes=createBrowserRouter([
-    {
-        path:"/",
-        element:<Home/>
-    },
-    // {
-    //     path:'/investor',
-    //     element:<Investor/>
-    // },
-    // {
-    //     path:"/Resto",
-    //     element:<resto/>
-    // }
-    {
-        path:'/log',
-        element:<Log/>
-    }
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <>
-   <Home/>
-   <Log/> 
+  {/* <Home/>
+   <Log/> */}
    
    
     </>
 );
-    <RouterProvider router={routes}/>
-)
+
