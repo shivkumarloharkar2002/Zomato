2253422364b65653b48d23fbc2fd6bf8c54c2cc8
pradnya <pradnya9022@gmail.com>
2024-05-27 01:32:08 -0600

    added code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Log from './views/Log/Log';
+import  { Log } from './views/Log/Log';
+import { Log2 } from './views/Log/Log2';
+import { Sign } from './views/Signup/Sign';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <>
    <Log/>
+   <Log2/>
+   <Sign/>
     </>
 );
---
+++ src/views/Signup/Sign.js
@@ -0,0 +1,10 @@
+import { Signup } from "../../components/Signup/Sign";
+
+export function Sign(){
+    return(
+        <>
+        <Signup/>
+        </>
+
+    )
+}
